9a0bade2cfb14f2290592d9772725979a52b4ca2
Stephan Köllen <stephan.koellen@korono.de>
2021-04-02 19:46:59 +0200

    set up package testing with phpunit and testbench

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Skoellen\LaravelProductManager\Tests;
+
+use SebastianBergmann\RecursionContext\InvalidArgumentException;
+use PHPUnit\Framework\ExpectationFailedException;
+
+class ExampleTest extends TestCase 
+{
+    public function test_example_test()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Skoellen\LaravelProductManager\Tests;
+
+use Orchestra\Testbench\TestCase as TestbenchTestCase;
+use Skoellen\LaravelProductManager\LaravelProductManagerServiceProvider;
+
+class TestCase extends TestbenchTestCase
+{
+    /**
+     * Get application timezone.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return string|null
+     */
+    protected function getApplicationTimezone($app)
+    {
+        return 'Europe/Berlin';
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            LaravelProductManagerServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function defineEnvironment($app)
+    {
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+}
